282174bc5a3ace4e1527f343f39459a5b92ddc17
Holger Langenau <holger.langenau@s2004.tu-chemnitz.de>
2011-05-20 18:27:24 +0200

    Autogen modifications

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "Running aclocal"
-aclocal
-echo "Running autoheader"
-autoheader
-echo "Running automake"
-automake
-echo "Running libtoolize"
-libtoolize
-echo "Running autoconf"
-autoconf
+automake --add-missing --copy
+autoreconf --install
+
+#echo "Running aclocal"
+#aclocal
+#echo "Running autoheader"
+#autoheader
+#echo "Running automake"
+#automake
+#echo "Running libtoolize"
+#libtoolize
+#echo "Running autoconf"
+#autoconf
